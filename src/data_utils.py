fix bug with Dataloader in Dataloader and with softmax being non-visible

src/data_utils.py
@@ -1,7 +1,7 @@
-from typing import List, Tuple, Optional, Union, Dict, Any
+from typing import Tuple, Optional, Union
 
 import numpy as np
-from sklearn.datasets import make_moons, fetch_openml
+from sklearn.datasets import fetch_openml
 
 
 
@@ -54,7 +54,7 @@ class DataLoader:
         return int(np.ceil(len(self.indices) / self.batch_size))
     
     @staticmethod
-    def holdout_split(dataset: Dataset, test_size: float = 0.2, batch_size: int = 32) -> Tuple['DataLoader', 'DataLoader']:
+    def holdout_split(dataset: Dataset, test_size: float = 0.2, batch_size: int = 32) -> Tuple['Dataset', 'Dataset']:
         """
         Splits the dataset into training and testing sets.
         Args:
@@ -70,8 +70,8 @@ class DataLoader:
         split_index = int(len(dataset) * (1 - test_size))
         train_indices = indices[:split_index]
         test_indices = indices[split_index:]
-        return DataLoader(dataset, train_indices, batch_size=batch_size, shuffle = True), DataLoader(dataset, test_indices, batch_size=batch_size, shuffle = False)
-   
+        #return DataLoader(dataset, train_indices, batch_size=batch_size, shuffle = True), DataLoader(dataset, test_indices, batch_size=batch_size, shuffle = False)
+        return Dataset(dataset.x[train_indices], dataset.y[train_indices]), Dataset(dataset.x[test_indices], dataset.y[test_indices]) 
    
 # function to generate synthetic dataset - spiral.
 

src/losses.py
@@ -4,7 +4,7 @@ from typing import List, Tuple, Optional, Union, Dict, Any
 import numpy as np
 
 
-def __softmax(x: np.ndarray) -> np.ndarray:
+def softmax(x: np.ndarray) -> np.ndarray:
     '''
     Compute softmax values for each sets of scores in x.
     x: 2D array of shape (n_samples, n_classes)
@@ -92,7 +92,7 @@ class CrossEntropySoftMax(Loss):
         '''
         assert y_true.shape == y_pred_logits.shape, f"True labels shape {y_true.shape} does not match predicted logits shape {y_pred_logits.shape}"
         #apply softmax to the predictions
-        probs = __softmax(y_pred_logits)
+        probs = softmax(y_pred_logits)
         loss = -np.sum(y_true * np.log(probs + 1e-15)) / y_true.shape[0]
         return loss, probs
 